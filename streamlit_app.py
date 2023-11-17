594d0791061e6e0c9f24704f4d20990d61bf0a41
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2023-11-17 02:59:39 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,36 +34,6 @@ def interactive_demo():
     st.header("Interactive Demo")
     st.write("Try out our interactive demo to see how the AI model can classify waste materials. Upload an image, and the AI will provide you with the classification results.")
 
-# Define the function for the "Waste Management" page with custom styling
-def waste_management():
-    st.header("Waste Management")
-    st.write("Efficient waste management is crucial for a sustainable future. Waste Watch helps waste management facilities automate the sorting process, reducing errors and improving recycling rates.")
-    # Add more information about waste management here
-
-# Define the function for the "About Us" page with custom styling
-def about_us():
-    st.header("About Us")
-
-    # Team member 1
-    st.subheader("John Doe")
-    st.image("https://intentplanning.ca/wp-content/uploads/2019/01/sample-person.jpg", caption="Project Lead", use_column_width=True)
-    st.text("John is passionate about AI and environmental sustainability. As the project lead, he oversees the development and implementation of Waste Watch.")
-
-    # Team member 2
-    st.subheader("Jane Smith")
-    st.image("https://intentplanning.ca/wp-content/uploads/2019/01/sample-person.jpg", caption="Machine Learning Engineer", use_column_width=True)
-    st.text("Jane specializes in machine learning and is responsible for training and fine-tuning the AI model for waste classification.")
-
-    # Team member 3
-    st.subheader("Bob Johnson")
-    st.image("https://intentplanning.ca/wp-content/uploads/2019/01/sample-person.jpg", caption="UX/UI Designer", use_column_width=True)
-    # st.text("Bob contributes to the project with his expertise in creating an intuitive and user-friendly interface for the Waste Watch application.")
-
-    # Add more team members as needed
-
-    # Contact information
-    st.subheader("Contact Us")
-    st.text("For inquiries or collaboration opportunities, please reach out to us at info@wastewatch.com.")
 
 # Set page configuration
 st.set_page_config(
@@ -103,7 +73,3 @@ if page == "Overview":
     overview_page()
 elif page == "Interactive Demo":
     interactive_demo()
-elif page == "Waste Management":
-    waste_management()
-elif page == "About Us":
-    about_us()
