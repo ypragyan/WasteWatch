6c98e9279e34fbc2ea3d68d796a8a968ec10ea47
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2023-11-19 19:11:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,46 +2,37 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from PIL import Image
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2, preprocess_input, decode_predictions
-
-# Load pre-trained MobileNetV2 model
-model = MobileNetV2(weights='imagenet')
 
 # Creating a simple Streamlit app
 def main():
-    st.title("Waste Classification App")
+    st.title("Simple Streamlit App")
 
     # Sidebar with user input
     user_input = st.sidebar.text_input("Enter your name", "John Doe")
     st.sidebar.write(f"Hello, {user_input}!")
 
     # Main content
-    st.header("Waste Image Classification")
+    st.header("Data Exploration")
 
-    # Upload an image through Streamlit
-    uploaded_file = st.file_uploader("Choose a waste image...", type=["jpg", "jpeg", "png"])
+    # Create a sample DataFrame
+    data = pd.DataFrame({
+        'Numbers': np.random.randn(100),
+        'Categories': np.random.choice(['A', 'B', 'C'], 100)
+    })
 
-    if uploaded_file is not None:
-        # Display the uploaded image
-        image_display = Image.open(uploaded_file)
-        st.image(image_display, caption="Uploaded Waste Image.", use_column_width=True)
+    # Display the DataFrame
+    st.write("Sample DataFrame:")
+    st.write(data)
 
-        # Convert the image to a format suitable for the model
-        img_array = image.img_to_array(image_display)
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array = preprocess_input(img_array)
+    # Plot a histogram of the 'Numbers' column
+    st.bar_chart(data['Numbers'])
 
-        # Make predictions using the pre-trained model
-        predictions = model.predict(img_array)
-        decoded_predictions = decode_predictions(predictions, top=1)[0][0]
-
-        # Display the waste classification result
-        st.subheader("Waste Classification Result:")
-        st.write(f"Predicted waste category: {decoded_predictions[1]}")
-        st.write(f"Confidence: {decoded_predictions[2]:.2%}")
+    # Display a map with random data points
+    map_data = pd.DataFrame(
+        np.random.randn(1000, 2) / [50, 50] + [37.7749, -122.4194],
+        columns=['lat', 'lon']
+    )
+    st.map(map_data)
 
 if __name__ == "__main__":
     main()
