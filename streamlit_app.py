4d246c0287fee7b20418a34c9c472f2943a46bad
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2023-11-19 19:39:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,27 +34,27 @@ def ai_page():
 
     # AI Model Architecture Image
     st.subheader("Model Architecture")
-    model_architecture_image =  Image.open("Images/wastewatch.jpg")
+    model_architecture_image =  Image.open("Images/Model.jpg")
     st.image(model_architecture_image, caption="WasteWatch Model Architecture", use_column_width=True)
 
     # Data Examples
-    st.subheader("Data Examples")
+    st.subheader("Results")
     data_example_images = [
-        Image.open("Images/wastewatch.jpg"),
-        Image.open("Images/wastewatch.jpg"),
+        Image.open("Images/Accuray.jpg"),
+        Image.open("Images/Loss.jpg"),
         Image.open("Images/wastewatch.jpg"),
     ]
     st.image(data_example_images, caption=["Example 1", "Example 2", "Example 3"], use_column_width=True)
 
     # Heatmaps
-    st.subheader("Heatmaps")
+    st.subheader("Attribution Methods: Visualising AI gained Knowledge")
     heatmap_images = [
-    Image.open("Images/wastewatch.jpg"),
-    Image.open("Images/wastewatch.jpg"),
-    Image.open("Images/wastewatch.jpg"),
-    Image.open("Images/wastewatch.jpg"),
+    Image.open("Images/Heatmap.jpg"),
+    Image.open("Images/Layers.jpg"),
+    Image.open("Images/XAI.jpg"),
+ 
     ]
-    st.image(heatmap_images, caption=["Heatmap 1", "Heatmap 2", "Heatmap 3", "Heatmap 4"], use_column_width=True)
+    st.image(heatmap_images, caption=["Heatmap 1", "Heatmap 2", "Heatmap 3"], use_column_width=True)
 
 # Main function
 def main():
