d6013631443c7f5dd39db38ac4552d800fce6bd1
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2023-11-19 19:06:15 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,52 +1,80 @@
 import streamlit as st
-# import tensorflow as tf
+
+# Set page configuration
+st.set_page_config(
+    page_title="Waste Watch - AI for Waste Classification",
+    page_icon="🗑️",
+    layout="wide"
+)
 
 
+# Define custom CSS for the pastel blue background
+custom_css = """
+<style>
+body {
+    background-color: #b2d8d8; /* Pastel Blue */
+}
+</style>
+"""
+
+# Use st.markdown to apply the custom CSS
+st.markdown(custom_css, unsafe_allow_html=True)
+
+# Your Streamlit app content goes here
+st.title("Pastel Blue Background")
+
+# Add more content here
+
+
+def overview_page():
+    st.title("Project Overview")
+    st.write("This is the overview page for your project.")
+    # Add your project overview content here
+
+# Create a function for another page
+def other_page():
+    st.title("Another Page")
+    st.write("This is another page of your Streamlit app.")
+    # Add content for this page here
+
+# Create a navigation menu
+page = st.sidebar.selectbox(
+    "Select a page",
+    ("Overview", "Interactive Demo", "Waste Management", "About Us")
+)
+
+# Define the function for the "Overview" page with custom styling
 def overview_page():
     st.header("Project Overview")
     st.write("Waste Watch is an innovative AI model that helps automate the process of waste classification and sorting. It is designed to assist waste management and recycling facilities in identifying and segregating various types of waste materials efficiently. Our goal is to contribute to environmental sustainability and make waste management more efficient.")
     # Add more project overview details here
+
 # Define the function for the "Interactive Demo" page with custom styling
 def interactive_demo():
+    st.header("Interactive Demo")
+    st.write("Try out our interactive demo to see how the AI model can classify waste materials. Upload an image, and the AI will provide you with the classification results.")
     # Add the interactive demo code here
-    upload= st.file_uploader('Insert image for classification', type=['png','jpg'])
-    c1, c2= st.columns(2)
-    if upload is not None:
-      im= Image.open(upload)
-      img= np.asarray(im)
-      image= cv2.resize(img,(128, 128))
-      img= preprocess_input(image)
-      img= np.expand_dims(img, 0)
-      c1.header('Input Image')
-      c1.image(im)
-      c1.write(img.shape)
-      #load weights of the trained model.
- 
+
 # Define the function for the "Waste Management" page with custom styling
- 
-# Set page configuration
-st.set_page_config(
-    page_title="WasteWatch",
-    page_icon="🗑️",
-    layout="wide"
-)
- 
-# Use st.markdown to apply the custom CSS
-# st.markdown(custom_css, unsafe_allow_html=True)
-# Front page layout
-st.title("Welcome to Waste Watch!")
-st.write("Empowering Waste Management with AI")
-# Display an engaging image or illustration
-st.image("Images/wastewatch.jpg", width= 100, caption="Image Source: Nidhi")
-# Add a call-to-action button
- 
-page = st.sidebar.selectbox(
-    "Select a page",
-    ("Overview", "Interactive Demo")
-)
+def waste_management():
+    st.header("Waste Management")
+    st.write("Efficient waste management is crucial for a sustainable future. Waste Watch helps waste management facilities automate the sorting process, reducing errors and improving recycling rates.")
+    # Add more information about waste management here
+
+# Define the function for the "About Us" page with custom styling
+def about_us():
+    st.header("About Us")
+    st.write("Waste Watch is a project developed for the NYAS Challenge. Our team is passionate about environmental sustainability and using AI for positive change. We are excited to contribute to the waste management and recycling industry.")
+    # Add team member details and contact information here
+
+
+
 # Display the selected page
 if page == "Overview":
     overview_page()
 elif page == "Interactive Demo":
     interactive_demo()
- 
+elif page == "Waste Management":
+    waste_management()
+elif page == "About Us":
+    about_us()
