49020319755c989279150f7fddda4efa445e3537
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2023-11-19 19:27:10 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,52 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Set page title and favicon
+st.set_page_config(page_title="WasteWatch", page_icon="♻️")
+
+# Function to load data
+def load_data():
+    # Replace this with your own data loading logic
+    data = pd.DataFrame({
+        'Date': pd.date_range(start='2023-01-01', periods=100, freq='D'),
+        'Waste Type': np.random.choice(['Plastic', 'Paper', 'Glass'], 100),
+        'Confidence': np.random.rand(100)
+    })
+    return data
+
+# Function to create a data table
+def display_data_table(data):
+    st.subheader("Results")
+    st.dataframe(data)
+
+# Function to create a sidebar with project information
+def project_sidebar():
+    st.sidebar.title("WasteWatch: AI Waste Sorting")
+    st.sidebar.info(
+        "WasteWatch is an AI-powered waste sorting project. It uses computer vision to classify waste types."
+    )
+    st.sidebar.subheader("Project")
+    st.sidebar.markdown("[GitHub Repository](https://github.com/ypragyan/WasteWatch/tree/main)")
+    st.sidebar.markdown("[Presentation](https://docs.google.com/presentation/d/1UNmV66jZEMEsP_Dm142TjZXEKSmYRRb8nxpoEgWEMis/edit#slide=id.g29cec67359b_0_0)")
+    st.sidebar.subheader("Contact Information")
+    st.sidebar.text("For inquiries, please contact:")
+    st.sidebar.text("Pragyan Yadav")
+    st.sidebar.text("ypragyan01@gmail.com")
+
+# Main function
+def main():
+    st.title("WasteWatch")
+
+    # Load data
+    data = load_data()
+
+    # Create layout
+    project_sidebar()
+
+    # Display project components
+    display_data_table(data)
+
+if __name__ == "__main__":
+    main()
  
