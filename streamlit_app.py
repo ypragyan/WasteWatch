73e1e1390f08f5eb79c2d4e7e4818e56b8f30047
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2023-11-19 19:29:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
 
 # Set page title and favicon
-st.set_page_config(page_title="WasteWatch", page_icon="♻️")
+st.set_page_config(page_title="WasteWatch: AI Waste Sorting", page_icon="♻️")
 
 # Function to load data
 def load_data():
@@ -15,38 +16,73 @@ def load_data():
     })
     return data
 
-# Function to create a data table
-def display_data_table(data):
-    st.subheader("Results")
-    st.dataframe(data)
-
 # Function to create a sidebar with project information
 def project_sidebar():
     st.sidebar.title("WasteWatch: AI Waste Sorting")
     st.sidebar.info(
         "WasteWatch is an AI-powered waste sorting project. It uses computer vision to classify waste types."
     )
-    st.sidebar.subheader("Project")
-    st.sidebar.markdown("[GitHub Repository](https://github.com/ypragyan/WasteWatch/tree/main)")
-    st.sidebar.markdown("[Presentation](https://docs.google.com/presentation/d/1UNmV66jZEMEsP_Dm142TjZXEKSmYRRb8nxpoEgWEMis/edit#slide=id.g29cec67359b_0_0)")
+    st.sidebar.subheader("Project Links")
+    st.sidebar.markdown("[GitHub Repository](https://github.com/yourusername/wastewatch)")
+    st.sidebar.markdown("[Documentation](https://wastewatchdocs.com)")
     st.sidebar.subheader("Contact Information")
     st.sidebar.text("For inquiries, please contact:")
-    st.sidebar.text("Pragyan Yadav")
-    st.sidebar.text("ypragyan01@gmail.com")
+    st.sidebar.text("Your Name")
+    st.sidebar.text("youremail@example.com")
+
+# Function to create the Problem page
+def problem_page():
+    st.title("Waste Sorting Problem")
+
+    st.write(
+        "The improper disposal of waste is a significant environmental concern. "
+        "Waste sorting is crucial to minimize the impact on the environment and enable recycling."
+    )
+
+    # Add more information about the waste sorting problem
+
+# Function to create the AI page
+def ai_page():
+    st.title("WasteWatch AI")
+
+    # AI Model Architecture Image
+    st.subheader("Model Architecture")
+    model_architecture_image = Image.open("model_architecture.png")
+    st.image(model_architecture_image, caption="WasteWatch Model Architecture", use_column_width=True)
+
+    # Data Examples
+    st.subheader("Data Examples")
+    data_example_images = [
+        Image.open("data_example_1.jpg"),
+        Image.open("data_example_2.jpg"),
+        Image.open("data_example_3.jpg")
+    ]
+    st.image(data_example_images, caption=["Example 1", "Example 2", "Example 3"], use_column_width=True)
+
+    # Heatmaps
+    st.subheader("Heatmaps")
+    heatmap_images = [
+        Image.open("heatmap_1.jpg"),
+        Image.open("heatmap_2.jpg"),
+        Image.open("heatmap_3.jpg"),
+        Image.open("heatmap_4.jpg")
+    ]
+    st.image(heatmap_images, caption=["Heatmap 1", "Heatmap 2", "Heatmap 3", "Heatmap 4"], use_column_width=True)
 
 # Main function
 def main():
-    st.title("WasteWatch")
-
     # Load data
-    data = load_data()
+    load_data()
 
     # Create layout
     project_sidebar()
 
-    # Display project components
-    display_data_table(data)
+    # Create navigation
+    pages = {"Problem": problem_page, "AI": ai_page}
+    selection = st.sidebar.radio("Select Page", list(pages.keys()))
+
+    # Display the selected page
+    pages[selection]()
 
 if __name__ == "__main__":
     main()
- 
