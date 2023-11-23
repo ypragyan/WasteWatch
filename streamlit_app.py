d0ae728ff56d879a7b399f878e4c9f8ed7912068
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2023-11-23 18:54:58 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
-import streamlit as st
-import tensorflow as tf
+# import tensorflow as tf
 from tensorflow import keras
 import requests
 import numpy as np
