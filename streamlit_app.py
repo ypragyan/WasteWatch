9ebad9bc5791cea1ef43c7cd3679681eec6b080c
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2023-11-23 18:11:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,6 @@
 import streamlit as st
 from PIL import Image
-import logging
-import logging.handlers
-import queue
-import threading
-import time
-import urllib.request
-import os
-from collections import deque
-from pathlib import Path
-from typing import List
-
  
-import numpy as np
-import pydub
-import streamlit as st
-from twilio.rest import Client
-import tensorflow
-
 
 # Set page title and favicon
 st.set_page_config(page_title="WasteWatch", page_icon="♻️")
