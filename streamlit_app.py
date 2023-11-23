092aef4b06abd8c3d79e8590c2f5f434ed9e7a9d
Pragyan Yadav <72915754+ypragyan@users.noreply.github.com>
2023-11-23 18:00:34 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
 from PIL import Image
+import streamlit as st
+import numpy as np
+from pandas import DataFrame
+from keybert import KeyBERT
+# For Flair (Keybert)
+from flair.embeddings import TransformerDocumentEmbeddings
+import seaborn as sns
+# For download buttons
+from functionforDownloadButtons import download_button
+import os
+import json
+
 
 # Set page title and favicon
 st.set_page_config(page_title="WasteWatch", page_icon="♻️")
